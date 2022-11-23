0fb00a39ae8a83cf85f23935a662987090cd86a6
Gautier PC Portable <g.mekhelian@gmail.com>
2022-11-23 14:33:44 +0100

    langue

---
+++ ProjetConsole/ProjetConsole/language.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesJson
+{
+        public class Language
+        {
+            public French? french;
+            public English? english;
+        }
+        public class French
+        {
+            public string? failed;
+            public string? sucess;
+        }
+        public class English
+        {
+            public string? failed;
+            public string? sucess;
+        }
+
+    }
