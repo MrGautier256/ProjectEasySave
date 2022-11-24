549e724b46f0ff80516b833686d9709c4213db1a
Gautier PC Portable <g.mekhelian@gmail.com>
2022-11-24 14:51:47 +0100

    Insertion de fonctionnalité de log
    Creation de classe JsonData, modification de savefile, creation de la fonction writelog éxcutée après la sauvegarde. Savefile créer des objetcs de type JsonData avec des infos de log, et writelog les injecte dans un fichier json a la fin du processus

---
+++ ProjetConsole/ProjetConsole/JsonData.cs
@@ -0,0 +1,28 @@
+﻿namespace TesJson
+{
+    public class JsonData
+    {
+        public JsonData(string _Name, string _SourceFilePath, string _TargetFilePath, int _FileNumber, 
+            int _TotalFilesToCopy, long _TotalFilesSize, int _NbFilesLeftToDo, float _Progression) 
+        {
+            Name= _Name;
+            SourceFilePath= _SourceFilePath;    
+            TargetFilePath= _TargetFilePath;    
+            FileNumber= _FileNumber;
+            TotalFilesToCopy= _TotalFilesToCopy;
+            TotalFilesSize= _TotalFilesSize;
+            NbFilesLeftToDo= _NbFilesLeftToDo;
+            Progression = _Progression;
+        }
+        public string Name { get; set; }
+        public string SourceFilePath { get; set; }
+        public string TargetFilePath { get; set; }
+        public int FileNumber { get; set; }
+        public int TotalFilesToCopy { get; set; }
+        public long TotalFilesSize { get; set; }
+        public int NbFilesLeftToDo { get; set; }
+        public float Progression { get; set; }
+
+    }
+
+}
--- ProjetConsole/ProjetConsole/JsonTraduction.cs
+++ ProjetConsole/ProjetConsole/JsonTraduction.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TesJson
+namespace ProjetConsole
 {
     public class JsonTraduction
     {
